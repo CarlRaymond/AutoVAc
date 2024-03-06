e17a56fe6784fef6b0ac6c0b87c3ef65d012de60
Carl Raymond <carl@orcharddev.com>
2024-03-05 23:09:18 -0500

    Swap bits for Code::C and Code::D

--- controller/src/codes.h
+++ controller/src/codes.h
@@ -7,10 +7,10 @@ enum class Code : unsigned char {
     START    = 0b1000,
     B        = 0b0100,
     STOP     = 0b0100,
-    C        = 0b0100,
-    D        = 0b1000,
+    C        = 0b0010,
+    D        = 0b0001,
     STARTING = 0b1011,
-    RUNNING  = 0b1001,
+    RUNNING  = 0b1010,
     MASK     = 0b1111,
 
     // Pseudo-codes corresponding to timer events
